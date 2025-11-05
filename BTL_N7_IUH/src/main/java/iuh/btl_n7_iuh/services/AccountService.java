5086542816f920422db520d6503655da5f37633f
dailoc77 <truongdailoc.dev@gmail.com>
2025-11-05 12:19:03 +0700

    done logic cart page

--- BTL_N7_IUH/src/main/java/iuh/btl_n7_iuh/services/AccountService.java
+++ BTL_N7_IUH/src/main/java/iuh/btl_n7_iuh/services/AccountService.java
@@ -1,14 +1,10 @@
 package iuh.btl_n7_iuh.services;
 
 import iuh.btl_n7_iuh.entities.Account; // Cập nhật import
-<<<<<<< HEAD
 import iuh.btl_n7_iuh.entities.AccountDetail;
 import iuh.btl_n7_iuh.entities.Role;
 import iuh.btl_n7_iuh.repositories.AccountRepository; // Cập nhật import
 import iuh.btl_n7_iuh.repositories.RoleRepository;
-=======
-import iuh.btl_n7_iuh.repositories.AccountRepository; // Cập nhật import
->>>>>>> 2ae1df9f312eac7306823a73ceed800f5fac9dfd
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +15,6 @@ public class AccountService {
 
     private final AccountRepository accountRepository; // Cập nhật Repository
     private final PasswordEncoder passwordEncoder;
-<<<<<<< HEAD
     private final RoleRepository roleRepository;
 
     public Account registerNewUser(Account account, String fullName, String phone, String address) {
@@ -44,13 +39,5 @@ public class AccountService {
 
         // ✅ Lưu account (Hibernate cascade ALL sẽ tự lưu AccountDetail)
         return accountRepository.save(account);
-=======
-
-    public void registerNewUser(Account account) { // Cập nhật tham số
-        account.setPassword(passwordEncoder.encode(account.getPassword()));
-        account.setIsEnabled(true);
-        // Có thể gán role mặc định ở đây nếu cần
-        accountRepository.save(account);
->>>>>>> 2ae1df9f312eac7306823a73ceed800f5fac9dfd
     }
 }
